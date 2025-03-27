3a1fa786af26e303a08564f468e42822df358052
Pedro Daniluz <pedrodaniluz04@gmail.com>
2025-03-27 15:33:26 -0300

    fix: label warning & change int price to float

--- src/components/PriceBox/index.jsx
+++ src/components/PriceBox/index.jsx
@@ -2,15 +2,14 @@ import '../InputField/InputField.css';
 
 export default function PriceBox({ id, title, value, width = 100 }) {
     const formatCurrency = (input) => {
-        // Garantir que o valor seja numérico e formatado como moeda
-        const num = parseInt(input, 10) || 0; // Converte para número, ou 0 se inválido
+        const num = parseFloat(input) || 0;
         return num.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     };
 
     return (
         <div className="textField" style={{ width: `${width}%` }}>
-            {title && <label htmlFor={id} className="textField--title">{title}</label>}
-            <span id={id} className="textField--field">{formatCurrency(value)}</span>
+            <label htmlFor={id} className="textField--title">{title}</label>
+            <input id={id} className="textField--field" value={formatCurrency(value)} readOnly/>
         </div>
     );
 }
