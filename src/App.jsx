c2d24b920d9d93e3570dc1d42f490cd70d5f8dbd
Pedro Daniluz <pedrodaniluz04@gmail.com>
2025-03-28 16:38:34 -0300

    feat: add routers + fix: navBar width shrink

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import Register from "./pages/Register"
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from "./pages/Register";
 
 function App() {
 
   return (
     <>
-      <Register/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   )
 }
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './NavBar.css'
 
 export default function NavBar() {
@@ -6,28 +7,28 @@ export default function NavBar() {
             <img src="/src/assets/images/pyterLogo.svg" alt="" className="logo"/>
             <ul>
                 <li>
-                    <a href="#">
+                    <Link to={'/'}>
                         <img src="/src/assets/images/icons/darkVariants/home.svg" alt="Ícone de casa"/>
                         <span>Home</span>
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="#">
+                    <Link to={''}>
                         <img src="/src/assets/images/icons/darkVariants/cart.svg" alt="Ícone de carrinho"/>
                         <span>Pedidos</span>
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="#">
+                    <Link to={'/register'}>
                         <img src="/src/assets/images/icons/darkVariants/add.svg" alt="Ícone de adição"/>
                         <span>Registrar Pedido</span>
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="#">
+                    <Link to={''}>
                         <img src="/src/assets/images/icons/darkVariants/store.svg" alt="Ícone de loja"/>
                         <span>Gerenciar Produtos</span>
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </nav>
