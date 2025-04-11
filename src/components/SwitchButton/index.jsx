5f6b665f178c160b8fc62a92021db4f97de2caa4
Pedro Daniluz <pedrodaniluz04@gmail.com>
2025-04-11 18:27:42 -0300

    feat: remove internal value controller

--- src/components/SwitchButton/index.jsx
+++ src/components/SwitchButton/index.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
 import '../../components/InputField/InputField.css'
 import './SwitchButton.css'
 
-export default function SwitchButton({ title, placeholder, onToggleChange, width = 100 }) {
-    const [toggled, setToggled] = useState(false);
-
+export default function SwitchButton({ title, placeholder, onToggleChange, width = 100, value = false }) {
     const handleToggle = () => {
-        setToggled((prev) => {
-            const newState = !prev;
-            if (onToggleChange) {
-                setTimeout(() => onToggleChange(newState), 0);
-            }
-            return newState;
-        });
+        if (onToggleChange) {
+            onToggleChange(!value);
+        }
     };
 
     return (
@@ -20,14 +13,14 @@ export default function SwitchButton({ title, placeholder, onToggleChange, width
             <span className='textField--title'>{title}</span>
             <div className='switch'>
                 <span>{placeholder}</span>
-                <button 
+                <button
                     type='button'
-                    className={`toggleBtn ${toggled? 'toggled' : ''}`}
+                    className={`toggleBtn ${value ? 'toggled' : ''}`}
                     onClick={handleToggle}
                 >
                     <div className='ball'></div>
                 </button>
             </div>
         </div>
-    )
+    );
 }
