dfd8ebf631c5b0a5da8420964806b62268bd247e
pedrodaniluz <pdaniluz.dev@gmail.com>
2025-04-20 21:15:58 -0300

    feat: update api baseURL to production server

--- src/services/Api/index.jsx
+++ src/services/Api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/"
+    baseURL: "http://54.211.52.179/"
 });
 
 export const getInstituicoes = async () => {
