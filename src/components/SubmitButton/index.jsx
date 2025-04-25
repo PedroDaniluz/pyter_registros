9ccc18c59c13a43aa23371fde6fdb7ab8cadd6ef
Pedro Daniluz <pedrodaniluz04@gmail.com>
2025-04-25 19:16:06 -0300

    feat: enhance SubmitButton component with icon support and dynamic styling

--- src/components/SubmitButton/index.jsx
+++ src/components/SubmitButton/index.jsx
@@ -1,7 +1,10 @@
 import './SubmitButton.css'
 
-export default function SubmitButton({ text, onclick }) {
-    return(
-        <button className='submit-button' onclick={onclick}>{text}</button>
+export default function SubmitButton({ text, onclick, type = 'fill', icon, marginTop }) {
+    return (
+        <button className={`submit-button ${type}`} onClick={onclick} style={{ marginTop:`${marginTop}` }}>
+            <span className='icon'>{ icon }</span>
+            <span>{ text }</span>
+        </button>
     )
 }
